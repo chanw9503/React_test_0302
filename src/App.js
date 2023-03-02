37802486aa9b949ba55a2aa45bd5727a49e875e8
ChanwooPark <chanw9503@gmail.com>
2023-03-02 10:35:29 +0900

    React test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const INIT_OBJUCT = {
+    id: 1,
+    content: '',
+  };
+  const [todoLists, setToDoList] = useState([INIT_OBJUCT]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap">
+      <AddFrom todoLists={todoLists} setToDoList={setToDoList} />
+      <div className="toDo_Container">
+        {todoLists.map((item) => (
+          <ToDoCard key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
+}
+
+function AddFrom({ todoLists, setToDoList }) {
+  const [todoContent, setTodoContent] = useState('');
+
+  function onSubmitList(e) {
+    e.preventDefault();
+    const newList = {
+      id: todoLists.length + 1,
+      content: todoContent,
+    };
+
+    setToDoList([...todoLists, newList]);
+  }
+  return (
+    <div className="container">
+      <form onSubmit={(e) => onSubmitList(e)}>
+        <input
+          onChange={(e) => setTodoContent(e.target.value)}
+          type={'text'}
+          required
+          placeholder="자바스크립트를 배워봅시다."
+        />
+        <input type={'submit'} value="추가하기" />
+      </form>
+      <h1>Todo List</h1>
+    </div>
+  );
+}
+
+function ToDoCard({ item }) {
+  console.log(item);
+  return <div className="toDoCard">가낟라</div>;
 }
 
 export default App;
